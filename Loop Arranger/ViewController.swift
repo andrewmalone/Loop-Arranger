348bdd97c0f2b17372f13a640a37bf9512349068
andrewmalone <andrew@andrewmalone.com>
2015-02-01 07:37:25 -0500

    Initial Commit

---
+++ Loop Arranger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loop Arranger
+//
+//  Created by Malone, Andrew P. on 2/1/15.
+//  Copyright (c) 2015 Andrew Malone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
